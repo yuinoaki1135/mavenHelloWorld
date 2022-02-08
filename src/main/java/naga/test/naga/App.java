d2fee4c27f676255c3a618d9e72efc96c670353a
yuinoaki1135 <yuinoaki1135@gmail.com>
2022-02-08 13:49:09 +0900

    first commit

---
+++ src/main/java/naga/test/naga/App.java
@@ -0,0 +1,13 @@
+package naga.test.naga;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/naga/test/naga/AppTest.java
@@ -0,0 +1,20 @@
+package naga.test.naga;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
